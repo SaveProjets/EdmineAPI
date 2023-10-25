206805745e1600de63e08058388ccc3e0eaff373
Ygroxie <ygroxie34@gmail.com>
2023-10-25 23:35:27 +0200

    Add AccountData and AccountInfo

--- src/fr/edminecoreteam/api/account/AccountData.java
+++ src/fr/edminecoreteam/api/account/AccountData.java
@@ -1,4 +1,185 @@
 package fr.edminecoreteam.api.account;
 
-public class AccountData {
+import fr.edminecoreteam.api.edorm.MySQL;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class AccountData
+{
+    private String p;
+
+    public AccountData(String p)
+    {
+        this.p = p;
+    }
+
+    public boolean hasRank()
+    {
+        try
+        {
+            PreparedStatement preparedStatement = MySQL.getConnection().prepareStatement("SELECT player_rank_id FROM ed_ranks WHERE player_name = ?");
+            preparedStatement.setString(1, p);
+            ResultSet rs = preparedStatement.executeQuery();
+            return rs.next();
+        }
+        catch (SQLException e)
+        {
+            e.toString();
+            return false;
+        }
+    }
+
+    public int getRankID()
+    {
+        try
+        {
+            PreparedStatement preparedStatement = MySQL.getConnection().prepareStatement("SELECT player_rank_id FROM ed_ranks WHERE player_name = ?");
+            preparedStatement.setString(1, p);
+            int power = 0;
+            ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next())
+            {
+                power = rs.getInt("player_rank_id");
+            }
+            preparedStatement.close();
+            return power;
+        }
+        catch (SQLException e)
+        {
+            e.toString();
+            return 0;
+        }
+    }
+
+    public String getRankType()
+    {
+        if (hasRank())
+        {
+            try
+            {
+                PreparedStatement preparedStatement = MySQL.getConnection().prepareStatement("SELECT player_rank_type FROM ed_ranks WHERE player_name = ?");
+                preparedStatement.setString(1, p);
+                String response = "";
+                ResultSet rs = preparedStatement.executeQuery();
+                while (rs.next())
+                {
+                    response = rs.getString("player_rank_type");
+                }
+                preparedStatement.close();
+                return response;
+            }
+            catch (SQLException e)
+            {
+                e.toString();
+            }
+        }
+        return "";
+    }
+
+    public String getPurchaseDate()
+    {
+        if (hasRank())
+        {
+            try
+            {
+                PreparedStatement preparedStatement = MySQL.getConnection().prepareStatement("SELECT player_rank_purchase_date FROM ed_ranks WHERE player_name = ?");
+                preparedStatement.setString(1, p);
+                String response = "";
+                ResultSet rs = preparedStatement.executeQuery();
+                while (rs.next())
+                {
+                    response = rs.getString("player_rank_purchase_date");
+                }
+                preparedStatement.close();
+                return response;
+            }
+            catch (SQLException e)
+            {
+                e.toString();
+            }
+        }
+        return "";
+    }
+
+    public String getDeadLineDate()
+    {
+        if (hasRank())
+        {
+            try
+            {
+                PreparedStatement preparedStatement = MySQL.getConnection().prepareStatement("SELECT player_rank_deadline_date FROM ed_ranks WHERE player_name = ?");
+                preparedStatement.setString(1, p);
+                String response = "";
+                ResultSet rs = preparedStatement.executeQuery();
+                while (rs.next())
+                {
+                    response = rs.getString("player_rank_deadline_date");
+                }
+                preparedStatement.close();
+                return response;
+            }
+            catch (SQLException e)
+            {
+                e.toString();
+            }
+        }
+        return "";
+    }
+
+    public int getRankModule()
+    {
+        if (hasRank())
+        {
+            try
+            {
+                PreparedStatement preparedStatement = MySQL.getConnection().prepareStatement("SELECT player_modulable_rank FROM ed_ranks WHERE player_name = ?");
+                preparedStatement.setString(1, p);
+                int response = 0;
+                ResultSet rs = preparedStatement.executeQuery();
+                while (rs.next())
+                {
+                    response = rs.getInt("player_modulable_rank");
+                }
+                preparedStatement.close();
+                return response;
+            }
+            catch (SQLException e)
+            {
+                e.toString();
+            }
+        }
+        return 0;
+    }
+
+    public String getRankName(){
+        try{
+            PreparedStatement ps = MySQL.getConnection().prepareStatement("SELECT player_rank_name FROM ed_ranks WHERE player_name = ?");
+            ps.setString(1, p);
+            String response = "§7Le §astaff";
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()){
+                response = rs.getString("player_rank_name");
+            }
+            if(response.equalsIgnoreCase("MOD")){
+                response = "§7Le §9modérateur";
+            }else if(response.equalsIgnoreCase("DEV")){
+                response = "§7Le §5développeur";
+            }else if (response.equalsIgnoreCase("RESP")){
+                response = "§7Le §4responsable";
+            }else if(response.equalsIgnoreCase("ADMIN")){
+                response = "§7L'§cadministrateur";
+            }else {
+                response = "§7Le §astaff";
+            }
+            ps.close();
+            return response;
+        }
+        catch (SQLException e){
+            e.toString();
+        }
+        return "§7Le §astaff";
+    }
+
 }
--- src/fr/edminecoreteam/api/account/AccountInfo.java
+++ src/fr/edminecoreteam/api/account/AccountInfo.java
@@ -1,4 +1,34 @@
 package fr.edminecoreteam.api.account;
 
-public class AccountInfo {
+import java.util.HashMap;
+import java.util.Map;
+
+public class AccountInfo
+{
+    private Map<String, AccountInfo> rankInfo;
+    private String p;
+    private AccountData accountData;;
+
+    public AccountInfo(String p) {
+        this.p = p;
+        this.rankInfo = new HashMap<String, AccountInfo>();
+        this.accountData = new AccountData(p);
+        this.rankInfo.put(p, this);
+    }
+
+    public AccountInfo getAccountInfos(String player) { return rankInfo.get(player); }
+
+    public boolean hasRank() { return accountData.hasRank(); }
+
+
+    public int getRankID() { return accountData.getRankID(); }
+
+    public int getRankModule() { return accountData.getRankModule(); }
+
+    public String getPurchaseDate() { return accountData.getPurchaseDate(); }
+
+    public String getDeadLineDate() { return accountData.getDeadLineDate(); }
+
+    public String getRankType() { return accountData.getRankType(); }
+    public String getRankName() { return accountData.getRankName(); }
 }
