450db916c6fa21deccd759d5a675c98cbec28507
Ygroxie <ygroxie34@gmail.com>
2024-03-03 15:11:36 +0100

    Add DB usage on Bungeecord and fix problem with DatabaseManager

--- src/main/java/fr/edminecoreteam/api/EdmineAPIBungee.java
+++ src/main/java/fr/edminecoreteam/api/EdmineAPIBungee.java
@@ -4,11 +4,20 @@ import fr.edminecoreteam.api.database.DatabaseManager;
 import fr.edminecoreteam.api.event.PlayerEventsBungee;
 import fr.edminecoreteam.api.utils.DBUtils;
 import net.md_5.bungee.api.plugin.Plugin;
-import org.bukkit.Bukkit;
+import net.md_5.bungee.config.Configuration;
+import net.md_5.bungee.config.ConfigurationProvider;
+import net.md_5.bungee.config.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
 
 public class EdmineAPIBungee extends Plugin {
     private static EdmineAPIBungee instance;
     private DBUtils dbUtils;
+
+    private static Configuration config;
 
     public static EdmineAPIBungee getInstance(){
         return instance;
@@ -19,6 +28,8 @@ public class EdmineAPIBungee extends Plugin {
         getLogger().info("EDMINEAPI enabling");
 
         instance=this;
+
+        loadConfig();
 
         getLogger().info("Loading managers...");
         this.dbUtils = new DBUtils();
@@ -34,6 +45,32 @@ public class EdmineAPIBungee extends Plugin {
         DatabaseManager.closeAllDatabaseConnections();
     }
 
+
+    private void loadConfig() {
+        if (!getDataFolder().exists()) {
+            getDataFolder().mkdir();
+        }
+        File configFile = new File(getDataFolder(), "config.yml");
+        if (!configFile.exists()) {
+            try (InputStream inputStream = getResourceAsStream("config.yml")) {
+                Files.copy(inputStream, configFile.toPath());
+            }
+            catch (IOException e) {
+                getLogger().warning("[EDMINE-API] Le fichier de config n'a pas pu être chargé... : " + e.getMessage());
+            }
+        }
+
+        // Charger la configuration depuis le fichier
+        try {
+            config = ConfigurationProvider.getProvider(YamlConfiguration.class).load(configFile);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Configuration getConfig(){
+        return config;
+    }
 
     public DBUtils getDbUtils(){return dbUtils;}
 }
--- src/main/java/fr/edminecoreteam/api/database/DatabaseManager.java
+++ src/main/java/fr/edminecoreteam/api/database/DatabaseManager.java
@@ -1,10 +1,20 @@
 package fr.edminecoreteam.api.database;
 
+import fr.edminecoreteam.api.EdmineAPIBungee;
 import fr.edminecoreteam.api.EdmineAPISpigot;
+import fr.edminecoreteam.api.utils.CheckServerType;
 
 public enum DatabaseManager {
 
-    EDMINE(new DatabaseCredentials(EdmineAPISpigot.getInstance().getConfig().getString("mysql.host"), EdmineAPISpigot.getInstance().getConfig().getString("mysql.user"), EdmineAPISpigot.getInstance().getConfig().getString("mysql.password"), EdmineAPISpigot.getInstance().getConfig().getString("mysql.database"), 3306));
+    EDMINE(getDatabaseCredential());
+
+    private static DatabaseCredentials getDatabaseCredential(){
+        if(CheckServerType.isSpigotServer()){
+            return new DatabaseCredentials(EdmineAPISpigot.getInstance().getConfig().getString("mysql.host"), EdmineAPISpigot.getInstance().getConfig().getString("mysql.user"), EdmineAPISpigot.getInstance().getConfig().getString("mysql.password"), EdmineAPISpigot.getInstance().getConfig().getString("mysql.database"), 3306);
+        }else{
+            return new DatabaseCredentials(EdmineAPIBungee.getInstance().getConfig().getString("mysql.host"), EdmineAPIBungee.getInstance().getConfig().getString("mysql.user"), EdmineAPIBungee.getInstance().getConfig().getString("mysql.password"), EdmineAPIBungee.getInstance().getConfig().getString("mysql.database"), 3306);
+        }
+    }
 
     private DatabaseAccess databaseAccess;
     DatabaseManager(DatabaseCredentials credentials){
@@ -20,8 +30,8 @@ public enum DatabaseManager {
             databaseManager.databaseAccess.initPool();
         }
     }
-    public static void closeAllDatabaseConnections(){
-        for(DatabaseManager databaseManager : values()){
+    public static void closeAllDatabaseConnections() {
+        for (DatabaseManager databaseManager : values()) {
             databaseManager.databaseAccess.closePool();
         }
     }
