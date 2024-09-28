aad6255c4970dcd47d21e792c183ef6213d140af
Ygroxie <ygroxie34@gmail.com>
2024-09-29 00:23:24 +0200

    add getDisplay on RankList

--- src/main/java/fr/edminecoreteam/api/management/list/RankList.java
+++ src/main/java/fr/edminecoreteam/api/management/list/RankList.java
@@ -1,11 +1,21 @@
 package fr.edminecoreteam.api.management.list;
 
+import java.util.HashMap;
+
 public enum RankList {
 
-    JOUEUR("§7[JOUEUR]"),
-    STAFF("§4[STAFF]");
+    JOUEUR("§7Joueur"),
+    VIP("§F§lVIP"),
+    SUPERVIP("§e§lSUPER-VIP"),
+    SUPREME("§a§lSUPREME"),
+    ULTRA("§bULTRA"),
+    ELITE("§3ELITE");
 
     private String display;
+
+    public String getDisplay() {
+        return display;
+    }
 
     RankList(String display) {
         this.display = display;
